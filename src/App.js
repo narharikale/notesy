63108f3bfd60a4e351176049056ecfc2280edf46
narharikale <naraharikale5051@gmail.com>
2022-03-29 10:53:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import "./App.css";
-import logo from "./logo.png";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
+    return (
+
+        <div>
+            tihs is not working
+            <img src="/assets/notesylogo.png" alt="no img"/>
+           
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
