b7731cfd94bf650f39a0386084e3fde145262ab9
narharikale <naraharikale5051@gmail.com>
2022-04-06 13:32:52 +0530

    fix : added go-to notes

--- src/frontend/screens/home/home.jsx
+++ src/frontend/screens/home/home.jsx
@@ -1,10 +1,13 @@
 
+import { Link } from 'react-router-dom';
 import './home.css'
 
 function Home() {
     return ( 
-        <div>
-
+        <div className='m-16'>
+        
+            <Link className="notsy-btn notesy-primary-btn" to='/notes'> Go to Notes </Link>
+            <h4> Homepage in construction   </h4>
 
         </div>
      );
